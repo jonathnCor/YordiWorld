0df151ec00e97d238677b49f9092b1c12399b380
jonathnCor <109770893+jonathnCor@users.noreply.github.com>
2024-01-13 00:44:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// For now, we will just add a simple script that could be used for handling video playbacks or other interactions
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    // Here we can attach event listeners to our video boxes if needed.
+});
